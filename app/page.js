612963dbfaefbcaf029fdcc746eee26bbfcbecf8
Nor Izza Acarina <norizzaafcarina63809@gmail.com>
2023-06-05 19:43:50 +0700

    starpoint section

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Testimoni from "./components/testimoni";
 import Contact from "./components/Contact";
 import Subcription_section from "./components/Subcription_section";
 import Footer from "./components/Footer";
+import Start_section from "./components/Start_section";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero_section></Hero_section>
       <Mission_section></Mission_section>
       <History></History>
+      <Start_section></Start_section>
       <Testimoni />
       <Subcription_section></Subcription_section>
       <Contact />
